966687ef2b8607d9d7911dc0b8ac5d8d9b8488ec
Justin G <ctuniseji@gmail.com>
2017-09-13 15:49:16 -0500

    working with materialize - adding temporary content into the website

---
+++ assets/js/init.js
@@ -0,0 +1,8 @@
+(function ($) {
+  $(function () {
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('#textarea1').trigger('autoresize');
+  }); // end of document ready
+})(jQuery); // end of jQuery name spaces
