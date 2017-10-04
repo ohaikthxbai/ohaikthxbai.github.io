7e5b22486df9ea20679d27d097787005ab53b967
ohaikthxbai <ctuniseji@gmail.com>
2017-10-03 22:23:16 -0500

    added connect link - regretted init edit

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,5 +4,4 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('#textarea1').trigger('autoresize');
-  }); // end of document ready
 })(jQuery); // end of jQuery name spaces
