7d1cd88cfe4769810c10855a538e6f4555535eb4
ohaikthxbai <ctuniseji@gmail.com>
2022-10-24 18:06:27 -0500

    add primevue and pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/tailwind-light/theme.css'; // theme
+import 'primevue/resources/primevue.min.css'; // core css
+import 'primeicons/primeicons.css'; // icons
+// component imports
+// import Button from 'primevue/button';
+// vue defaults
 import App from './App.vue';
 import router from './router';
-import store from './store';
+// import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(createPinia()).use(router).use(PrimeVue);
+
+// app.component('Button', Button);
+
+app.mount('#app');
