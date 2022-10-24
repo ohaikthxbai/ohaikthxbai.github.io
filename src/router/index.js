4ecd2bf243a46cd3f197b6b23bd9fd11001ce8a5
ohaikthxbai <ctuniseji@gmail.com>
2022-10-24 18:45:26 -0500

    contact me addition

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // route level code-splitting
+    // this generates a separate chunk (contact.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue'),
+  },
 ];
 
 const router = createRouter({
