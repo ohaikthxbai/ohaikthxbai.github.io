a1b6d1ff37027f231b9ddaf6ee2218a8a12e1dde
ohaikthxbai <ctuniseji@gmail.com>
2022-10-25 12:36:47 -0500

    install primeflex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
 import 'primevue/resources/themes/tailwind-light/theme.css'; // theme
 import 'primevue/resources/primevue.min.css'; // core css
 import 'primeicons/primeicons.css'; // icons
